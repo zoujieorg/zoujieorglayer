3143d35a6de4100c54467fd6a14f8da02d3759a4
zoujieorg <zoujieorg@gmail.com>
2016-10-25 20:55:27 +0800

    添加了C-w,M-w单行操作

--- config.el
+++ config.el
@@ -1,8 +1,8 @@
 (global-linum-mode t)
 ;;(load-theme 'monokai t)
 (set-language-environment "UTF-8")
-(setq initial-frame-alist '((fullscreen . maximized)))
-(setq-default cursor-type 'bar)
+;;(setq initial-frame-alist '((fullscreen . maximized)))
+;;(setq-default cursor-type 'bar)
 ;;选择文本内容时，如有键入，则选择内容被替换
 (delete-selection-mode t)
 
@@ -14,3 +14,16 @@
 
 ;;(require 'gradle-mode)
 ;;(gradle-mode 1)
+(add-hook 'org-mode-hook (lambda () (setq truncate-lines nil)))
+
+;; C-w 来删除整行（不用事先选中整行），同时不影响原有的剪切功能。M-w 也可以在不事先选中整行的情况下复制整行。
+(defadvice kill-ring-save (before slickcopy activate compile)
+  (interactive
+   (if mark-active (list (region-beginning) (region-end))
+     (list (line-beginning-position)
+           (line-beginning-position 2)))))
+(defadvice kill-region (before slickcut activate compile)
+  (interactive
+   (if mark-active (list (region-beginning) (region-end))
+     (list (line-beginning-position)
+           (line-beginning-position 2)))))
